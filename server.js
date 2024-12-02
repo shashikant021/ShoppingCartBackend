6b8e63bacb16bcf9c67e41ca2c5e8cfd9a7105b3
shashikant021 <shashikantsahu487@gmail.com>
2024-12-02 12:23:22 +0530

    Backend started

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://shashikantsahu487:SahuShoppingCart@cluster0.ujdp1.mongodb.net/"
+  )
+  .then(() => console.log("Connected to mongodb successfully"))
+  .catch((error) => console.log(error));
+
+  app.use(cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization","Cache-Control", "Expires", 'Pragma'],
+    credentials: true //credentials is use for login and register. 
+  }));
+
+  app.use(express.json());
+  app.use(cookieParser());
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
