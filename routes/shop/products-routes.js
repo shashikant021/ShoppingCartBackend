9c6273b2401824daf404c049ae33d3ea74ad5760
shashikant021 <shashikantsahu487@gmail.com>
2024-12-23 19:31:36 +0530

    orders Details Done

--- routes/shop/products-routes.js
+++ routes/shop/products-routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const {
-  getFilteredProducts
+  getFilteredProducts,
+  getProductDetails
 } = require("../../controllers/shop/products-controller");
 
 const router = express.Router();
 
 router.get("/get", getFilteredProducts);
+router.get("/get/:id", getProductDetails);
+
 
 module.exports = router;
