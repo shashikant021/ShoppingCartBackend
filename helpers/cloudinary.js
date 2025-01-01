5026f092342a98bb7ffa8990ce28d221caafac5b
shashikant021 <shashikantsahu487@gmail.com>
2025-01-01 14:45:06 +0530

    all changes done

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dtd348l5l",
-  api_key: "235365636649694",
-  api_secret: "TPSSKRXrU9Vr2ncsqlOgpUIRe4s",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
